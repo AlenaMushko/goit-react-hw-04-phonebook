359dde8bb008017b9b7d16fa8d813bd3adddbbc1
alena <evstafienko_a@ukr.net>
2023-03-03 21:11:07 +0200

    add src

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './styles/index.css';
+import './styles/theme.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+     <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
